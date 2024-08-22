c004306aaf493cb609470b93dcf9d714358a55a8
DanPaul12 <dan.paul.schechter@gmail.com>
2024-08-22 15:35:51 -0700

    hw24

--- cosmos.py
+++ cosmos.py
@@ -1,3 +1,17 @@
 import requests
 import json
 
+def fetch_planet_data():
+    url = "https://api.le-systeme-solaire.net/rest/bodies/"
+    response = requests.get(url)
+    planets = response.json()['bodies']
+
+    #process each planet info
+    for planet in planets:
+        if planet['isPlanet']:
+            name = #get planet English name
+            mass = #get planet mass
+            orbit_period = #get planet orbit period
+            print(f"Planet: {name}, Mass: {mass}, Orbit Period: {orbit_period} days")
+
+fetch_planet_data()
